c23677a0fc9e9da3349c53a93467d93098a1d818
a8u8akar007 <i232589@isb.nu.edu.pk>
2024-12-07 17:06:09 +0500

    Integrated new methods and resolved linking issues

--- congestion.h
+++ congestion.h
@@ -9,10 +9,38 @@
 
 #define INT_MAX 2147483647      //Bohot bara number
 
+struct heapNode
+{
+    int num_of_vehicles;
+    heapNode* left;
+    heapNode* right;
+    char start_intersection;
+    char end_intersection;
+    heapNode(int num_of_vehicles, char start_intersection, char end_intersection);
+};
+
+class MinHeap
+{
+private:
+    heapNode* root;
+    void heapifyup(heapNode* current);
+    int countnodes(heapNode* current);
+    heapNode* findnode(heapNode* current, int index, int& count);
+    heapNode* findnode(heapNode* root, int index);
+    heapNode* findParent(heapNode* current, heapNode* child);
+
+public:
+    MinHeap();
+    void insert(int num_of_vehicles, char start_intersection, char end_intersection);
+    void custructMinHeap(node** matrix, int num_vertices);
+    heapNode* exractMaxNode(heapNode*& root);
+    heapNode* FindMostCongestedRoad();
+    void display();
+};
+
 class Vehicles {
 private:
     node** matrix;
-    int** roadUsage;
     int num_vertices;
 
 public:
@@ -25,6 +53,7 @@ public:
     void printRoadUsage();
     void findShortestPathDijkstra(int start, int end, int* pathTaken);
     void printPath(int* pathTaken, int start, int end);
+    void NumOfVehicles(string csv);
     void findAllPathsHelper(int current, int end, bool* visited, int* path, int path_index, bool& pathFound);
     void findAllPaths(char startc, char endc);
 };
