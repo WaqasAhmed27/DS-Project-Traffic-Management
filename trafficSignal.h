a4bce8cb6721429113282496de1521c36dd1970f
Waqas Ahmed <i232540@isb.nu.edu.pk>
2024-12-07 16:17:07 +0500

    Added logic for green light timing based on congestion
    Parity change with trafficSignal.cpp

---
+++ trafficSignal.h
@@ -0,0 +1,25 @@
+#ifndef TRAFFICSIGNAL_H
+#define TRAFFICSIGNAL_H
+
+#include "trafficNetwork.h"
+#include "congestion.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class TrafficSignal {
+private:
+    char intersections[1000] = {};  // Contains intersections
+    int greenTimes[1000] = {};       // Contains green light timings
+    int totalSignals;
+
+public:
+    TrafficSignal();
+    void readTrafficSignalData(string csv);
+    void adjustGreenTimes(TrafficNetwork& network, string vehiclesCSV, int vertices, Vehicles vehicles);
+    void displayGreenTimes(int a, TrafficNetwork& network);
+};
+
+#endif
